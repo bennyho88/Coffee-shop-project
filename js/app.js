220a3c37fafc8ca7612455a59f4b1f5fd5d7f517
Benny Ho <benny.ho2530@gmail.com>
2020-03-28 20:39:01 +0100

    added preloader js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// hide preloader
+// all the images scripts  links have finished loading
+
+
+// window event list
+
+window.addEventListener('load', function() {
+
+    document.querySelector('.preloader').style.display = 'none';
+})
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+
+   
+
+    setTimeout(function() {
+        document.querySelector('.preloader').style.display = 'none';
+        
+    }, 3000)
+ 
+   
+})
+*/
