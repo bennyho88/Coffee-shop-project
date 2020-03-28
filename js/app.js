4c9205c175f1ab96481ce13bb0b18ea89f554c70
Benny Ho <benny.ho2530@gmail.com>
2020-03-28 22:20:35 +0100

    added eventlistener function and refactored with constructor function

--- js/app.js
+++ js/app.js
@@ -2,42 +2,64 @@
 // all the images scripts  links have finished loading
 
 
-// window event list
 
+eventListeners();
 
-window.addEventListener('load', function() {
+function eventListeners() {
 
+    const ui = new UI();
+
+    // window event list
+
+    window.addEventListener('load', function () {
+
+        ui.hidePreLoader();
+
+    })
+
+    //nav btn
+    document.querySelector('.navBtn').addEventListener('click', function () {
+
+       ui.showNav();
+    })
+}
+
+// constructor function
+
+function UI() {
+    
+}
+
+UI.prototype.hidePreLoader = function() {
     document.querySelector('.preloader').style.display = 'none';
+}
 
-})
+UI.prototype.showNav = function() {
+    document.querySelector('.nav').classList.toggle('nav--show');
+}
 
-//nav btn
 
-document.querySelector('.navBtn').addEventListener('click', function() {
 
-   /* document.querySelector('.nav').classList.toggle('nav--show') */
-   document.querySelector('.nav').classList.toggle('nav--show');
-})
 
 
 
 /*
 document.addEventListener('DOMContentLoaded', function() {
 
-   
+
 
     setTimeout(function() {
         document.querySelector('.preloader').style.display = 'none';
-        
+
     }, 2000)
 
 
     document.querySelector('.navBtn').addEventListener('click', function() {
-        
-        
+
+
         document.querySelector('.nav').classList.toggle('nav__btn');
         console.log('it works')
-   
+
 })
 
 })
