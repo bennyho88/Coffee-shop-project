12e60603d70e7f12e76aa8937c64cf70b34475fd
bennyho88 <49682891+bennyho88@users.noreply.github.com>
2020-05-26 22:08:50 +0200

    modification
    modified html text and added margin css

--- js/app.js
+++ js/app.js
@@ -1,209 +1,177 @@
-// hide preloader
-// all the images scripts  links have finished loading
-
-
-
 eventListeners();
 
 function eventListeners() {
+  const ui = new UI();
 
-    const ui = new UI();
+  // window event list
 
-    // window event list
+  window.addEventListener("load", function () {
+    ui.hidePreLoader();
+  });
 
-    window.addEventListener('load', function () {
+  //nav btn
+  document.querySelector(".navBtn").addEventListener("click", function () {
+    ui.showNav();
+  });
 
-        ui.hidePreLoader();
+  // control the video
 
-    })
+  document
+    .querySelector(".video__switch")
+    .addEventListener("click", function () {
+      ui.videoControls();
+    });
 
-    //nav btn
-    document.querySelector('.navBtn').addEventListener('click', function () {
+  //submit the form
 
-        ui.showNav();
-    })
+  document
+    .querySelector(".drink-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const name = document.querySelector(".input-name").value;
+      const lastName = document.querySelector(".input-lastname").value;
+      const email = document.querySelector(".input-email").value;
 
-    // control the video
+      let value = ui.checkEmpty(name, lastName, email);
 
-    document.querySelector('.video__switch').addEventListener('click', function () {
+      if (value) {
+        let customer = new Customer(name, lastName, email);
+        console.log(customer);
+        ui.addCustomer(customer);
+        ui.showFeedback("customer added to the list", "success");
+        ui.clearFields();
+      } else {
+        ui.showFeedback("some form values empty", "error");
+      }
+    });
 
-        ui.videoControls();
-    })
+  // display modal
 
-    //submit the form
+  const links = document.querySelectorAll(".work-item__icon");
 
-    document.querySelector('.drink-form').addEventListener('submit', function (event) {
+  links.forEach(function (item) {
+    item.addEventListener("click", function (event) {
+      ui.showModal(event);
+    });
+  });
+  // hide modal
 
-        event.preventDefault();
-        const name = document.querySelector('.input-name').value;
-        const lastName = document.querySelector('.input-lastname').value;
-        const email = document.querySelector('.input-email').value;
-
-        let value = ui.checkEmpty(name, lastName, email);
-
-        if (value) {
-
-            let customer = new Customer(name, lastName, email);
-            console.log(customer)
-            ui.addCustomer(customer);
-            ui.showFeedback('customer added to the list', 'success');
-            ui.clearFields();
-        } else {
-            ui.showFeedback('some form values empty', 'error');
-        }
-    })
-
-    // display modal
-
-    const links = document.querySelectorAll('.work-item__icon');
-    
-    links.forEach( function(item) {
-
-        item.addEventListener('click', function(event) {
-            ui.showModal(event);
-        })
-    })
-    // hide modal
-
-    document.querySelector('.work-modal__close').addEventListener('click', function() {
-
-        ui.closeModal();
-    })
+  document
+    .querySelector(".work-modal__close")
+    .addEventListener("click", function () {
+      ui.closeModal();
+    });
 }
 
 // constructor function
 
-function UI() {
-
-}
+function UI() {}
 
 // hide preloader
 UI.prototype.hidePreLoader = function () {
-    document.querySelector('.preloader').style.display = 'none';
-}
+  document.querySelector(".preloader").style.display = "none";
+};
 
 // show Nav
 UI.prototype.showNav = function () {
-    document.querySelector('.nav').classList.toggle('nav--show');
-}
-
+  document.querySelector(".nav").classList.toggle("nav--show");
+};
 
 // play/pause the video
 
 UI.prototype.videoControls = function () {
+  let btn = document.querySelector(".video__switch-btn");
 
-    let btn = document.querySelector('.video__switch-btn');
-
-    if (!btn.classList.contains('btnSlide')) {
-        btn.classList.add('btnSlide');
-        document.querySelector('.video__item').pause()
-    } else {
-        btn.classList.remove('btnSlide');
-        document.querySelector('.video__item').play()
-    }
-
-}
-
+  if (!btn.classList.contains("btnSlide")) {
+    btn.classList.add("btnSlide");
+    document.querySelector(".video__item").pause();
+  } else {
+    btn.classList.remove("btnSlide");
+    document.querySelector(".video__item").play();
+  }
+};
 
 UI.prototype.checkEmpty = function (name, lastName, email) {
+  let result;
 
-    let result;
+  if (name === "" || lastName === "" || email === "") {
+    result = false;
+  } else {
+    result = true;
+  }
 
-    if (name === '' || lastName === '' || email === '') {
-        result = false;
-    } else {
-        result = true;
-    }
-
-    return result;
-}
+  return result;
+};
 
 UI.prototype.showFeedback = function (text, type) {
+  const feedback = document.querySelector(".drink-form__feedback");
 
-    const feedback = document.querySelector('.drink-form__feedback');
-
-    if (type === 'success') {
-
-
-        feedback.classList.add('success');
-        feedback.innerText = text;
-        this.removeAlert('success');
-
-    } else if (type === 'error') {
-
-        feedback.classList.add('error');
-        feedback.innerText = text;
-        this.removeAlert('error');
-    }
-}
+  if (type === "success") {
+    feedback.classList.add("success");
+    feedback.innerText = text;
+    this.removeAlert("success");
+  } else if (type === "error") {
+    feedback.classList.add("error");
+    feedback.innerText = text;
+    this.removeAlert("error");
+  }
+};
 
 UI.prototype.removeAlert = function (type) {
-
-    setTimeout(function () {
-        document.querySelector('.drink-form__feedback').classList.remove(type);
-    }, 3000)
-}
+  setTimeout(function () {
+    document.querySelector(".drink-form__feedback").classList.remove(type);
+  }, 3000);
+};
 
 // add customer
 UI.prototype.addCustomer = function (customer) {
-
-    const images = [1, 2, 3, 4, 5];
-    let random = Math.floor(Math.random() * images.length);
-    console.log(random)
-    const div = document.createElement('div');
-    div.classList.add('person');
-    div.innerHTML = `
+  const images = [1, 2, 3, 4, 5];
+  let random = Math.floor(Math.random() * images.length);
+  console.log(random);
+  const div = document.createElement("div");
+  div.classList.add("person");
+  div.innerHTML = `
     <img src="img/person-${random}.jpeg" alt="" class="person__thumbnail">
             <h4 class="person-name">${customer.name}</h4>
             <h4 class="person-last-name">${customer.lastname}</h4>
-    `
+    `;
 
-    document.querySelector('.drink-card__list').appendChild(div);
-}
+  document.querySelector(".drink-card__list").appendChild(div);
+};
 
 // clear fields
 
 UI.prototype.clearFields = function () {
-
-    document.querySelector('.input-name').value = '';
-    document.querySelector('.input-lastname').value = '';
-    document.querySelector('.input-email').value = '';
-}
+  document.querySelector(".input-name").value = "";
+  document.querySelector(".input-lastname").value = "";
+  document.querySelector(".input-email").value = "";
+};
 // show modal
 
-UI.prototype.showModal = function(event) {
+UI.prototype.showModal = function (event) {
+  event.preventDefault();
 
-    event.preventDefault();
+  if (event.target.parentElement.classList.contains("work-item__icon")) {
+    let id = event.target.parentElement.dataset.id;
 
-    if(event.target.parentElement.classList.contains('work-item__icon')) {
+    const modal = document.querySelector(".work-modal");
+    const modalItem = document.querySelector(".work-modal__item");
 
-        let id = event.target.parentElement.dataset.id;
-        
-        const modal = document.querySelector('.work-modal');
-        const modalItem = document.querySelector('.work-modal__item');
+    modal.classList.add("work-modal--show");
 
-        modal.classList.add('work-modal--show');
-        
-       modalItem.style.backgroundImage = `url(img/work-${id}.jpeg)`
-
-
-    }
-}
+    modalItem.style.backgroundImage = `url(img/work-${id}.jpeg)`;
+  }
+};
 
 // hide modal
 
-UI.prototype.closeModal = function() {
-
-    document.querySelector('.work-modal').classList.remove('work-modal--show');
-}
+UI.prototype.closeModal = function () {
+  document.querySelector(".work-modal").classList.remove("work-modal--show");
+};
 // customer
 
 function Customer(name, lastname, email) {
-
-    this.name = name;
-    this.lastname = lastname;
-    this.email = email;
+  this.name = name;
+  this.lastname = lastname;
+  this.email = email;
 }
-
-
-
